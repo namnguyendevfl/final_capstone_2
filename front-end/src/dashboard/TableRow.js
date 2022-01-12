b162bfb8e20fc62e5ccaa4f4f6162eea9d4fc77f
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-12 12:11:23 -0500

    done with US-5

--- front-end/src/dashboard/TableRow.js
+++ front-end/src/dashboard/TableRow.js
@@ -16,8 +16,6 @@ export default function TableRow({ table }) {
 			<th scope="row">{table.table_id}</th>
 			<td>{table.table_name}</td>
 			<td>{table.capacity}</td>
-			
-			{ /* the instructions say the tests are looking for this data-table-id-status, so be sure to include it. */ }
 			<td data-table-id-status={table.table_id}>{table.status}</td>
             {table.status === "occupied" &&
 			<td data-table-id-finish={table.table_id}>
