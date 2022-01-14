6329d15542547d23c8b66b3ffb9de2226dfc35f5
namnguyendevfl <namnguyenfl990@gmail.com>
2022-01-14 13:27:03 -0500

    done with US-7-backend

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,12 +2,18 @@ const knex = require("../db/connection");
 
 const tableName = "reservations";
 
-function list(date) {
+function list(date, mobile_number) {
 	if(date) {
 		return knex(tableName)
 			.select("*")
 			.where({ reservation_date: date })
 			.orderBy("reservation_time", "asc");
+	}
+
+	if(mobile_number) {
+		return knex(tableName)
+			.select("*")
+			.where('mobile_number', 'like', `${mobile_number}%`);
 	}
 
 	return knex(tableName)
